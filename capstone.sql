28232ff2d85a67b7afc88bd1fe24dd15258ffbf7
Fatima740227 <fatima3.iscool@gmail.com>
2026-07-24 17:33:45 -0800

    initial commit

--- capstone.sql
+++ capstone.sql
@@ -51,3 +51,46 @@ VALUES
 ("28",2500,'8-4-2026',"2008","1004");
 
 SELECT * FROM orders;
+
+SELECT Customer.customer_name, salesman.name, salesman.city
+FROM Customer 
+Join Salesman ON Customer.city = Salesman.city;
+
+SELECT Customer.customer_name, Salesman.name
+FROM Customer 
+JOIN Salesman ON Customer.salesman_id = Salesman.salesman_id
+
+SELECT Orders.order_no, Customer.customer_name,Orders.customer_id, Orders.salesman_id
+FROM Orders
+JOIN Customer ON Orders.customer_id = Customer.customer_id
+JOIN Salesman ON Orders.salesman_id = Salesman.salesman_id
+WHERE Customer.city <> Salesman.city;
+
+SELECT Orders.order_no, Customer.customer_name
+FROM Orders
+JOIN Customer ON Orders.customer_id = Customer.customer_id;
+
+SELECT Customer.customer_name AS "Customer", Customer.grade AS "Grade"
+FROM Orders
+JOIN Salesman ON Orders.Salesman_id = Salesman.salesman_id
+JOIN Customer ON Orders.customer_id = Customer.customer_id
+WHERE Customer.grade IS NOT NULL;
+
+SELECT Customer.customer_name AS "Customer",
+Customer.city AS "city",
+Salesman.name AS "Salesman",
+Salesman.commision
+FROM Customer
+JOIN Salesman ON Customer.salesman_id = Salesman.salesman_id
+WHERE Salesman.commision BETWEEN 0.15 AND 0.31;
+
+SELECT Orders.order_no, Customer.customer_name, Salesman.commision AS "Commission"
+FROM Orders
+JOIN Salesman ON Orders.salesman_id = Salesman.salesman_id
+JOIN Customer ON Orders.customer_id = Customer.customer_id
+WHERE Customer.grade >= 100;
+
+SELECT *
+FROM Customer
+JOIN Orders ON Customer.customer_id = Orders.customer_id
+WHERE Orders.order_date = '16-7-2026';
